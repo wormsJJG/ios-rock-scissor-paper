1002a99ba68603ef61f4c9fe249cdf645404f24d
wormsJJG <jjr66461837@gmail.com>
2022-08-16 15:33:42 +0900

    feat: step1 구현

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+func startGame(){
+    print("가위(1), 바위(2), 보(3)!<종료 : 0>:", terminator: " ")
 
+    let userInputNumber: Int = Int(readLine() ?? "-1") ?? -1
+    let computerNumber: Int = Int.random(in: 1...3)
+    
+    dicisionWinOrLose(userInputNumber, computerNumber)
+}
+
+func dicisionWinOrLose(_ human: Int, _ computer: Int) {
+    if human == computer {
+        print("비겼습니다!")
+        startGame()
+    } else if human == 1 && computer == 2 || human == 2 && computer == 3 || human == 3 && computer == 1 {
+        print("졌습니다!")
+    } else if human == 1 && computer == 3 || human == 2 && computer == 1 || human == 3 && computer == 2 {
+        print("이겼습니다!")
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
+    }
+}
+
+startGame()
