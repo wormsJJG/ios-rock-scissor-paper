223ceb32dc18e6f6ef14b467e592e9aa97f046bc
wormsJJG <jjr66461837@gmail.com>
2022-08-16 15:36:10 +0900

    fix: 게임 종료 구현

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -24,6 +24,8 @@ func dicisionWinOrLose(_ human: Int, _ computer: Int) {
         print("졌습니다!")
     } else if human == 1 && computer == 3 || human == 2 && computer == 1 || human == 3 && computer == 2 {
         print("이겼습니다!")
+    } else if human == 0 {
+        print("게임 종료")
     } else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         startGame()
